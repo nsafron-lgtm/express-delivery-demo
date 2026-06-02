02b560a194f0be005a0335a3b302995bfef0df9c
nsafron-lgtm <nsafron@cleverence.com>
2026-06-02 16:57:54 +0400

    Add project config files and GitHub Actions workflow

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/express-delivery-demo/' : '/',
+  server: { host: "::", port: 8080 },
+  plugins: [react()],
+  resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
+}));
